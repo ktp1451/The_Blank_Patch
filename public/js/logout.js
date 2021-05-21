618d64fafafe4424b4628cdcd6e049e55970edfc
Daniel Manley <dmen1002@msn.com>
2021-05-21 13:21:41 -0500

    changed user login info

---
+++ public/js/logout.js
@@ -0,0 +1,30 @@
+const logout = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to log out.');
+  }
+};
+
+document.querySelector('#logout').addEventListener('click', logout);
+
+
+const logoutMobile = async () => {
+  const response = await fetch('/api/users/logout', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+  });
+  
+  if (response.ok) {
+    document.location.replace('/');
+  } else {
+    alert('Failed to log out.');
+  }
+};
+
+document.querySelector('#logout-mobile').addEventListener('click', logout);
